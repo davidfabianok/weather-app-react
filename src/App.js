114fcb49fdc0d23a50431f0aa22d4a9c6e90d274
David Fabian <david.fabian@phinxlab.com>
2020-11-25 02:18:30 -0300

    adding prettierrc and cityInfo changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CityInfo from './components/CityInfo/CityInfo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <CityInfo city="Salta" country="Argentina"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
