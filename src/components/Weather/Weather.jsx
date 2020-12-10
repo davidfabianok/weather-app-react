fcf4a76334868ed144364712fc54bce620c3f75c
David Fabian <david.fabian@phinxlab.com>
2020-12-10 08:21:04 -0300

    refactor weather

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
-import { WiCloud, WiDayCloudy, WiDayFog, WiDaySunny, WiRain } from 'react-icons/wi';
+import IconState, { validValues } from '../IconState';
 import { IconContext } from 'react-icons';
 
-const stateByName = {
-  cloud: WiCloud,
-  cloudy: WiDayCloudy,
-  fog: WiDayFog,
-  sunny: WiDaySunny,
-  rain: WiRain,
-};
-
-const renderState = (state) => {
-  const IconState = stateByName[state] || stateByName['sunny'];
-  return <IconState />;
-};
 
 const Weather = ({ temperature, state }) => (
   <div>
-    <IconContext.Provider value={{ size: '5em' }}>{renderState(state)}</IconContext.Provider>
+    <IconContext.Provider value={{ size: '5em' }}>
+          <IconState state={state} />
+        </IconContext.Provider>
     <Typography display="inline" variant="h3">
       {temperature}
     </Typography>
   </div>
 );
-
-const validValues = ['cloud', 'cloudy', 'fog', 'sunny', 'rain'];
 
 Weather.propTypes = {
   temperature: PropTypes.number.isRequired,
